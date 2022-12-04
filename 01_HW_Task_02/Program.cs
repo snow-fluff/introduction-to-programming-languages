3f3698e80586c2b9c4f188ae865fd2adcb1a92b2
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-12-04 12:58:09 +0300

    add homework task 02

--- 01_HW_Task_02/Program.cs
+++ 01_HW_Task_02/Program.cs
@@ -3,4 +3,17 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите первое число: ");
+int a1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int a2 = int.Parse(Console.ReadLine()!);
+
+if (a1 > a2)
+{
+    Console.WriteLine("Число " + a1 + " больше чем число " + a2);
+}
+else
+{
+    Console.WriteLine("Число " + a2 + " больше чем число " + a1);
+}
