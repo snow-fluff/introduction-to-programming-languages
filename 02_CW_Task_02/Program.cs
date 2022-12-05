9a81141a5879289a98eba2408f042274eeca30f2
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-12-05 20:57:37 +0300

    add classwork lection 2 task 02

---
+++ 02_CW_Task_02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Использование функции и массивов при нахождении MAX из 9 элементов
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+//               0   1   2   3   4   5   6   7   8   
+int[] array = { 12, 32, 34, 29, 34, 87, 34, 67, 32 };
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
