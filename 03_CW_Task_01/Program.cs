a2485e8003353f9b8f863c344ff3b8b851f0cf1a
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-12-11 10:14:15 +0300

    add classwork lection 3 task 01

---
+++ 03_CW_Task_01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Дан текст. В нём нужно все пробелы заменить чёрточками, маленькие буквы «к»
+// заменить большими «К», а большие «С» заменить на маленькие «с»
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
++ "ежели бы вас послали вместо нашего милого Винценгероде,"
++ "вы бы взяли приступом согласие прусского короля."
++ "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char OldValue, char NewValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == OldValue) result = result + $"{NewValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '_');
+newText = Replace(newText, 'к', 'К');
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
