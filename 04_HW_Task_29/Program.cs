913494914c9a8043895469313b3b587da1c94b61
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-12-12 20:56:46 +0300

    add homework seminar 4 task 29

---
+++ 04_HW_Task_29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] arr)
+{
+    for (int index = 0; index < arr.Length; index++)
+    {
+        arr[index] = new Random().Next(1, 1000);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int index = 0; index < arr.Length; index++)
+    {
+        Console.Write(arr[index] + " ");
+    }
+}
+
+Console.Write("Введите размер массива: ");
+int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+
+FillArray(array);
+PrintArray(array);
