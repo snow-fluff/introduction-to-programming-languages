734803af25064c3257ae97904a2ad2926ef235f8
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-12-12 20:35:34 +0300

    add homework seminar 4 task 27

---
+++ 04_HW_Task_27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int sumStructure(int A)
+{
+    int res = 0;
+    if (A < 10)
+    {
+        res = A;
+    }
+    else
+    {
+        while (A > 10)
+        {
+            res = res + A % 10;
+            A = A / 10;
+        }
+        res = res + A;
+    }
+    return res;
+}
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write(numA + " -> " + sumStructure(A: numA));
