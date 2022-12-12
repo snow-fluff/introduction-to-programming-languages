ade99a20dcc2b6b118cb3cb9990c63cc30f1a12b
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-12-12 20:20:13 +0300

    add homework seminar 4 task 25

---
+++ 04_HW_Task_25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int inPower(int A, int B)
+{
+    int res = A;
+    for (int i = 2; i <= B; i++)
+    {
+        res = res * A;
+    }
+    return res;
+}
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write(numA + " в степени " + numB + " -> ");
+
+Console.Write(inPower(A: numA, B: numB));
