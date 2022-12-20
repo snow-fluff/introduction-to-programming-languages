3664362a37cf1c21578607ced1d19bf869e8a147
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-12-20 20:21:45 +0300

    add homework seminar 5 task 34

---
+++ 05_HW_Task_34/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void FillArray(int[] arr)
+{
+    for (int index = 0; index < arr.Length; index++)
+    {
+        arr[index] = new Random().Next(100, 999);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[ ");
+    for (int index = 0; index < arr.Length; index++)
+    {
+        Console.Write(arr[index] + " ");
+    }
+    Console.Write("] ");
+}
+
+int CountEven(int[] arr)
+{
+    int count = 0;
+    for (int index = 0; index < arr.Length; index++)
+    {
+        if (arr[index] % 2 == 0)
+        {
+            count++;
+        };
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+
+FillArray(array);
+PrintArray(array);
+Console.Write("-> " + CountEven(array));
