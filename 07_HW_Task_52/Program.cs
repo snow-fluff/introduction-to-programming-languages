acd581f2a0eaae1c228dc7926f5729f9fe392b5f
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-12-22 22:25:10 +0300

    add homework seminar 7 task 52

---
+++ 07_HW_Task_52/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+Console.Write("Введите количество строк двумерного массива: ");
+int numRow = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов двумерного массива: ");
+int numCol = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[numRow, numCol];
+FillArray(array);
+PrintArray(array);
+
+Console.Write("Среднее арифметическое каждого столбца: ");
+decimal average;;
+for (int j = 0; j < numCol; j++)
+{
+    average = 0;
+    for (int i = 0; i < numRow; i++)
+    {
+        average = average + array[i,j];
+    }
+    Console.Write("{0: 0.0}", average/numRow);
+    Console.Write(" ");
+}
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 9);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
