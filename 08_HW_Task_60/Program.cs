85bbc3c11bffbe5e177c3aaeaae4596e230002fb
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-12-25 21:57:31 +0300

    add homework seminar 8 task 60

---
+++ 08_HW_Task_60/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.Clear();
+Console.Write("Задайте порядок трехмерного массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[,,] array = new int[arraySize, arraySize, arraySize];
+
+FillArray(array);
+PrintArrayByString(array);
+
+void FillArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[i, j, k] = new Random().Next(10, 99);
+            }
+        }
+    }
+}
+
+void PrintArrayByString(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($"{arr[i, j, k]} " + "(" + i + "," + j + "," + k + ") ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
