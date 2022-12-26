36a5f55ce696581a58c80f662d9f611effa593f6
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-12-26 21:43:23 +0300

    add homework seminar 9 task 66

---
+++ 09_HW_Task_66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.Write("Введите натураьное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натураьное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("M = " + m + "; N = " + n + " -> " + FindNaturalNumbersSumm(m, n));
+
+int FindNaturalNumbersSumm(int numM, int numN)
+{
+    if (numM == (numN - 1)) return (numM + numN);
+    else return (numM + FindNaturalNumbersSumm(numM + 1, numN));
+}
