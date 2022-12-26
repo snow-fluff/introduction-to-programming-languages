443e80b1a0c40a4a65188a633b0a3971537e5a0e
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-12-26 21:33:59 +0300

    add homework seminar 9 task 64

---
+++ 09_HW_Task_64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+Console.Write("Введите натураьное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("N = " + n + " -> " + FindNaturalNumbers(n));
+
+string FindNaturalNumbers(int num)
+{
+    if (num == 1) return num.ToString();
+    else return (num + ", " + FindNaturalNumbers(num-1));
+}
